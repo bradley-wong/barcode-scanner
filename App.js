201bba5c5d5df82ed10adb26b2fe7baea986c17e
John Whangbo <whangbok@hotmail.com>
2019-03-30 16:33:38 -0700

    Added multiple screens

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 // import { Camera } from "expo"
 import BarcodeScannerExample from './components/BarcodeScanner';
+import HomeScreen from './components/HomeScreen';
+import SecondScreen from './components/SecondScreen';
+
+const RootStack = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen
+        },
+        Details: {
+            screen: SecondScreen
+        }
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+
+const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <BarcodeScannerExample />
-      // <View style={styles.container}>
-      //   <Camera
-      //     ratio="16:9"
-      //     style={{ flex: 1, justifyContent: "center" }}
-      //     >
-      //     <View style={{ flex: 1, justifyContent: "center" }}><Text>hello</Text></View>
-      //   </Camera>
-      // </View >
-    );
-  }
+    render() {
+        return <AppContainer />;
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
