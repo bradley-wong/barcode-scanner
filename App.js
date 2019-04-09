bb60206ae3f3af474b7d2c8acaaf93ab3447c62c
JasonZFHuang <jasonhuangzf@gmail.com>
2019-04-09 13:54:35 -0700

    Registration

--- App.js
+++ App.js
@@ -7,37 +7,42 @@ import BarcodeScanner from './view/BarcodeScanner';
 import Home from './view/Home';
 import AddItem from './view/AddItem';
 import ListItem from './view/ListItem';
+import Login from './view/Login'
+
 
 const RootStack = createStackNavigator(
-    {
-        Home: {
-            screen: Home
-        },
-        Add: {
-            screen: AddItem
-        },
-        List: {
-            screen: ListItem
-        },
-        Barcode: {
-            screen: BarcodeScanner
-        },
+  {
+    Login: {
+      screen: Login
     },
-    {
-        initialRouteName: 'Home',
-    }
+    Home: {
+      screen: Home
+    },
+    Add: {
+      screen: AddItem
+    },
+    List: {
+      screen: ListItem
+    },
+    Barcode: {
+      screen: BarcodeScanner
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  }
 );
 
 const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-    render() {
-        return <AppContainer />;
-    }
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 });
---
+++ view/Login.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { Alert, View, StyleSheet, Text, Button, TextInput, TouchableHighlight } from 'react-native';
+
+import { db } from '../db';
+import { registerAcc } from '../service/MyServiceInterface';
+
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      password: '',
+      error: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handlePChange = this.handlePChange.bind(this)
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+
+
+  static navigationOptions = {
+    title: 'Login',
+    headerStyle: {
+      backgroundColor: '#F59BAD',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  handleChange(e) {
+    console.log(this.state.name)
+    this.setState({
+      name: e.nativeEvent.text
+    })
+  }
+
+  handlePChange(e) {
+    console.log(this.state.password)
+    this.setState({
+      password: e.nativeEvent.text
+    })
+  }
+  handleRegister() {
+    registerAcc(this.state.name, this.state.password)
+      .then(r => Alert.alert(r))
+      .catch(e => Alert.alert(e))
+  }
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <Text style={styles.title}>Register/Log In</Text>
+        <TextInput
+          style={styles.itemInput}
+          onChange={this.handleChange}
+          placeholder={"Username"} />
+        <TextInput
+          style={styles.itemInput}
+          onChange={this.handlePChange}
+          placeholder={"Password"}/>
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor="white"
+          onPress={this.handleRegister} >
+          <Text style={styles.title}>Register</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    padding: 30,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#FAD0D5'
+  },
+  title: {
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  list: {
+    fontSize: 14
+  },
+  itemInput: {
+    height: 50,
+    padding: 4,
+    marginTop: 10,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#111",
+    alignSelf: 'center'
+  },
+  button: {
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderRadius: 8,
+    marginBottom: 5,
+    marginTop: 5,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
